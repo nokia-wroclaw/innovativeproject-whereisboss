563609a6b80e02b972f36434294d5aaa45250eab
unknown <martamstowska@gmail.com>
2015-01-26 13:50:08 +0100

    poprawione wyswietlanie mapy, przy starej konwencji json

--- Android/WhereIsMyBoss/app/src/main/java/com/example/baksu/whereismyboss/ActivitySearch.java
+++ Android/WhereIsMyBoss/app/src/main/java/com/example/baksu/whereismyboss/ActivitySearch.java
@@ -5,9 +5,29 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.graphics.Bitmap;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.graphics.BitmapFactory;
 
 /**
  * Created by Baksu on 2014-12-13.
@@ -17,19 +37,131 @@ public class ActivitySearch extends Activity {
     private ServerTransmission serverTransmission;
     private Context context;
     ImageView map_image;
+    ImageView image;
+    private TextView text;
+    WebView myWebView;
+    Bitmap bitMap;
 
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
 
-        context = getApplicationContext();
-        map_image = (ImageView)findViewById(R.id.main_imagemap);
+
+        image = (ImageView) findViewById(R.id.main_imagemap);
+
+
+        String image_url = "https://s3.amazonaws.com/whereisboss-assets/54bf6f4244a96b0300d33cbe.jpeg";
+        SmartImageView myImage = (SmartImageView) this.findViewById(R.id.my_image);
+        myImage.setImageUrl(image_url);
+        myImage.setImageContact(contactAddressBookId);
+
+
+
+
+
+       /* myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl(ShowMap.url);
+
+        Bitmap btmp = myWebView.getDrawingCache(true);
+        image.setImageBitmap(btmp);
+
+
+       // myWebView.getSettings().setBuiltInZoomControls(true);
+       // myWebView.setVerticalScrollBarEnabled(true);
+      //  myWebView.setHorizontalScrollBarEnabled(true);
+
+         Toast.makeText(ActivitySearch.this, ShowMap.url, Toast.LENGTH_SHORT).show();
+       // myWebView.setScrollContainer(true);
+
+        image = (ImageView)findViewById(R.id.main_imagemap);
+
+        Toast.makeText(ActivitySearch.this, ShowMap.data, Toast.LENGTH_SHORT).show();
+
 
         Intent service = new Intent(this, ServerTransmission.class);
         bindService(service, bService, this.BIND_AUTO_CREATE);
 
-    }
+
+
+
+
+        text = (TextView) findViewById(R.id.editText);
+        final Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                // Toast.makeText(ActivitySearch.this, text.getText().toString(), Toast.LENGTH_SHORT).show();
+                String user = text.getText().toString();
+
+                if(user.equals("q")) {
+
+
+                    ShowMap.data = "{\"rooms\":[{\"name\":\"RED\",\"coordinates\":[[342,38],[342,207],[578,206],[580,37]]},{\"name\":\"YELLOW\",\"coordinates\":[[682,427],[683,530],[831,526],[828,426]]},{\"name\":\"BLUE\",\"coordinates\":[[345,350],[344,471],[467,472],[468,348]]}]}";
+                    //ShowMap.data = "{\"building\":\"Lab\",\"floor\":\"0\",\"mapa\":\"https://s3.amazonaws.com/whereisboss-assets/54bf6f4244a96b0300d33cbe.jpeg\",\"rooms\":[{\"_id\":\"54bf6f6444a96b0300d33cbf\",\"coordinates\":[[54,237],[52,387],[229,392],[228,236]],\"room\":\"Lab 1\"},{\"_id\":\"54bf6f6444a96b0300d33cc0\",\"coordinates\":[[228,236],[230,393],[457,392],[456,235]],\"room\":\"Lab 2\"},{\"_id\":\"54bf6f6444a96b0300d33cc1\",\"coordinates\":[[456,235],[457,392],[633,393],[633,234]],\"room\":\"Lab 3\"}]\"location\":\"Lab 2\"}";
+                    ShowMap.room = "RED";
+                    // {\"building\":\"Lab\",\"floor\":\"0\",\"mapa\":\"https://s3.amazonaws.com/whereisboss-assets/54bf6f4244a96b0300d33cbe.jpeg\",\"rooms\":[{\"_id\":\"54bf6f6444a96b0300d33cbf\",\"coordinates\":[[54,237],[52,387],[229,392],[228,236]],\"room\":\"Lab 1\"},{\"_id\":\"54bf6f6444a96b0300d33cc0\",\"coordinates\":[[228,236],[230,393],[457,392],[456,235]],\"room\":\"Lab 2\"},{\"_id\":\"54bf6f6444a96b0300d33cc1\",\"coordinates\":[[456,235],[457,392],[633,393],[633,234]],\"room\":\"Lab 3\"}]\"location\":\"Lab 2\"}";
+
+                    // ShowMap showMap = (ShowMap) findViewById(R.id.main_imagemap);
+                    // showMap.setVisibility(View.GONE);
+                    ShowMap.url = "https://s3.amazonaws.com/whereisboss-assets/54bf6f4244a96b0300d33cbe.jpeg";
+                    //myWebView = (WebView) findViewById(R.id.webview);
+                    //  myWebView.loadUrl(url);
+
+                    //map_image = (ImageView) findViewById(R.id.main_imagemap);
+                    //myWebView.loadUrl(url);
+                   Intent refresh = new Intent(ActivitySearch.this, ActivitySearch.class);
+                      finish();
+                      startActivity(getIntent());
+
+
+
+
+
+
+
+                    // setContentView(R.layout.search_activity);
+                    //  ShowMap showMap = (ShowMap) findViewById(R.id.main_imagemap);
+                    //  map_image = (ImageView)findViewById(R.id.main_imagemap);
+                    //  Intent refresh = new Intent(ActivitySearch.this, ActivitySearch.class);
+                    //  finish();
+                    //  startActivity(getIntent());
+
+
+                    //  Canvas canvas = new Canvas();
+                    //  showMap.onDraw(canvas);
+                    //context = getApplicationContext();
+                    // map_image = (ImageView)findViewById(R.id.main_imagemap);
+                    //Toast.makeText(ActivitySearch.this, "wszedl", Toast.LENGTH_SHORT).show();
+                    // ShowMap.bool = true;
+                    // Intent refresh = new Intent(ActivitySearch.this, ActivitySearch.class);
+                    //startActivityForResult(refresh, GlobalDataStore.STATIC_INTEGER_VALUE);
+                    //  finish();
+                    //  startActivity(getIntent());
+
+
+
+                }
+
+                else {
+
+
+                }
+
+
+
+
+            }
+
+        });*/
+
+
+
+
+
+
 
     /**
      * Metoda odpowiedzialna za obsługę przyciusku back
@@ -40,6 +172,7 @@ public class ActivitySearch extends Activity {
         this.unbindService(bService);
         this.finish();
     }
+
 
     /**
      * Metoda odpowiedzialna za podpiecie serwisu do Activity
--- Android/WhereIsMyBoss/app/src/main/java/com/example/baksu/whereismyboss/ShowMap.java
+++ Android/WhereIsMyBoss/app/src/main/java/com/example/baksu/whereismyboss/ShowMap.java
@@ -12,16 +12,22 @@ import android.graphics.Path;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import android.graphics.Typeface;
+import android.widget.Toast;
 
 
 public class ShowMap extends ImageView {
 
     //parametry wejsciowe
-    String data = "{\"rooms\":[{\"name\":\"RED\",\"coordinates\":[[342,38],[342,207],[578,206],[580,37]]},{\"name\":\"YELLOW\",\"coordinates\":[[682,427],[683,530],[831,526],[828,426]]},{\"name\":\"BLUE\",\"coordinates\":[[345,350],[344,471],[467,472],[468,348]]}]}";
-    String room = "RED";
+    public static String data = "";
+    public static String room = "";
+    public static String url = "";
+
+
+
 
     public ShowMap(Context context) {
         super(context);
+
         // TODO Auto-generated constructor stub
     }
 
@@ -38,139 +44,147 @@ public class ShowMap extends ImageView {
     @Override
     protected void onDraw(Canvas canvas) {
 
-        int maxX;
-        int minX;
-        int maxY;
-        int minY;
-        JSONObject dataJSON;
 
-        String coordinatesString;
-        Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
-        Path polyPath = new Path();
-
-        p.setStyle(Paint.Style.STROKE);
-        super.onDraw(canvas);
-        try {
-            //na potrzeby testowania zamiana na JSONObject, a to powinno być parametrem funkcji
-            dataJSON =  new JSONObject(data);
+            int maxX;
+            int minX;
+            int maxY;
+            int minY;
+            JSONObject dataJSON;
 
 
-            JSONArray array = dataJSON.getJSONArray("rooms");
-            JSONObject[] rooms = new JSONObject[array.length()];
 
-            for (int i=0; i < array.length(); i++) {
-                p.setStrokeWidth(4);
-                maxX = 0;
-                minX = 0;
-                maxY = 0;
-                minY = 0;
+            String coordinatesString;
+            Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+            Path polyPath = new Path();
 
-                //wyluskanie stringa samych wspolrzednych, dla kazdego pokoju osobno
-                rooms[i] = array.getJSONObject(i);
-                System.out.println(rooms[i].getString("name"));
-                coordinatesString = rooms[i].getString("coordinates");
-                coordinatesString = rooms[i].getString("coordinates");
-                coordinatesString = coordinatesString.substring(2, coordinatesString.length()-2);
-                String[] coordinatesArray = coordinatesString.split(",");
-                //obrobka tych wspolrzednych, aby pozostala sama liczba
-                for (int j=0; j < coordinatesArray.length; j++) {
-                    if(coordinatesArray[j].startsWith("[")) {
-                        coordinatesArray[j] = coordinatesArray[j].substring(1);
-                    }
-                    else if(coordinatesArray[j].endsWith("]")) {
-                        coordinatesArray[j] = coordinatesArray[j].substring(0,coordinatesArray[j].length()-1);
-                    }
-                }
-                //pozycjonowanie napisu oraz zaznaczanie obrysu obszarow
-                maxX = Integer.parseInt(coordinatesArray[0].toString());
-                minX = Integer.parseInt(coordinatesArray[0].toString());
-                maxY = Integer.parseInt(coordinatesArray[1].toString());
-                minY = Integer.parseInt(coordinatesArray[1].toString());
-                for (int j=0; j < coordinatesArray.length; j++) {
-                    if(j==0) {
-                        polyPath.moveTo(Integer.parseInt(coordinatesArray[j].toString()),Integer.parseInt(coordinatesArray[j+1].toString()));
-                    }
-                    else {
-                        polyPath.lineTo(Integer.parseInt(coordinatesArray[j].toString()),Integer.parseInt(coordinatesArray[j+1].toString()));
+            p.setStyle(Paint.Style.STROKE);
+            super.onDraw(canvas);
 
-                        if( Integer.parseInt(coordinatesArray[j].toString())> maxX ){
-                            maxX = Integer.parseInt(coordinatesArray[j].toString());
-                        }
-                        if( Integer.parseInt(coordinatesArray[j].toString()) < minX ){
-                            minX = Integer.parseInt(coordinatesArray[j].toString());
-                        }
-                        if( Integer.parseInt(coordinatesArray[j+1].toString()) > maxY ){
-                            maxY = Integer.parseInt(coordinatesArray[j+1].toString());
-                        }
-                        if( Integer.parseInt(coordinatesArray[j+1].toString())< minY ){
-                            minY = Integer.parseInt(coordinatesArray[j+1].toString());
+            try {
+                //na potrzeby testowania zamiana na JSONObject, a to powinno być parametrem funkcji
+                dataJSON = new JSONObject(data);
+
+
+                JSONArray array = dataJSON.getJSONArray("rooms");
+                JSONObject[] rooms = new JSONObject[array.length()];
+
+                for (int i = 0; i < array.length(); i++) {
+                    p.setStrokeWidth(4);
+                    maxX = 0;
+                    minX = 0;
+                    maxY = 0;
+                    minY = 0;
+
+                    //wyluskanie stringa samych wspolrzednych, dla kazdego pokoju osobno
+                    rooms[i] = array.getJSONObject(i);
+                    System.out.println(rooms[i].getString("name"));
+                    coordinatesString = rooms[i].getString("coordinates");
+                    coordinatesString = rooms[i].getString("coordinates");
+                    coordinatesString = coordinatesString.substring(2, coordinatesString.length() - 2);
+                    String[] coordinatesArray = coordinatesString.split(",");
+                    //obrobka tych wspolrzednych, aby pozostala sama liczba
+                    for (int j = 0; j < coordinatesArray.length; j++) {
+                        if (coordinatesArray[j].startsWith("[")) {
+                            coordinatesArray[j] = coordinatesArray[j].substring(1);
+                        } else if (coordinatesArray[j].endsWith("]")) {
+                            coordinatesArray[j] = coordinatesArray[j].substring(0, coordinatesArray[j].length() - 1);
                         }
                     }
-                    j++;
-                }
-                p.setColor(0xdd222222);
-                p.setStyle(Paint.Style.FILL);
-                p.setStrokeWidth(1);
-                p.setTextSize(18);
-                p.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
-                canvas.drawText(rooms[i].getString("name"), (minX + maxX)/2 - ((maxX-minX)/4), (minY + maxY)/2,p);
 
-                p.setStyle(Paint.Style.STROKE);
-                p.setStrokeWidth(3);
-                polyPath.lineTo(Integer.parseInt(coordinatesArray[0].toString()),Integer.parseInt(coordinatesArray[1].toString()));
-                canvas.drawPath(polyPath, p);
-                polyPath.close();
-            }
-        }
-        catch(Throwable t) {}
 
-        //zakreslanie wyszczegolnionego pomieszczenia w parametrze funkcji
-        Paint p2 = new Paint(Paint.ANTI_ALIAS_FLAG);
-        Path polyPath2 = new Path();
-        try {
-            dataJSON =  new JSONObject(data);
-            JSONArray array = dataJSON.getJSONArray("rooms");
-            JSONObject[] rooms = new JSONObject[array.length()];
+                    //pozycjonowanie napisu oraz zaznaczanie obrysu obszarow
+                    maxX = Integer.parseInt(coordinatesArray[0].toString());
+                    minX = Integer.parseInt(coordinatesArray[0].toString());
+                    maxY = Integer.parseInt(coordinatesArray[1].toString());
+                    minY = Integer.parseInt(coordinatesArray[1].toString());
+                    for (int j = 0; j < coordinatesArray.length; j++) {
+                        if (j == 0) {
+                            polyPath.moveTo(Integer.parseInt(coordinatesArray[j].toString()), Integer.parseInt(coordinatesArray[j + 1].toString()));
+                        } else {
+                            polyPath.lineTo(Integer.parseInt(coordinatesArray[j].toString()), Integer.parseInt(coordinatesArray[j + 1].toString()));
 
-            for (int i=0; i < array.length(); i++) {
-                p2.setStrokeWidth(4);
-                p2.setStyle(Paint.Style.FILL);
-
-                rooms[i] = array.getJSONObject(i);
-                coordinatesString = rooms[i].getString("coordinates");
-                coordinatesString = rooms[i].getString("coordinates");
-                coordinatesString = coordinatesString.substring(2, coordinatesString.length()-2);
-                String[] coordinatesArray = coordinatesString.split(",");
-
-                for (int j=0; j < coordinatesArray.length; j++) {
-
-                    if(coordinatesArray[j].startsWith("[")) {
-                        coordinatesArray[j] = coordinatesArray[j].substring(1);
-                    }
-                    else if(coordinatesArray[j].endsWith("]")) {
-                        coordinatesArray[j] = coordinatesArray[j].substring(0,coordinatesArray[j].length()-1);
-                    }
-                }
-                if(rooms[i].getString("name").equals(room)) {
-                    for (int j=0; j < coordinatesArray.length; j++) {
-                        if(j==0) {
-                            polyPath2.moveTo(Integer.parseInt(coordinatesArray[j].toString()),Integer.parseInt(coordinatesArray[j+1].toString()));
-                        }
-                        else {
-                            polyPath2.lineTo(Integer.parseInt(coordinatesArray[j].toString()),Integer.parseInt(coordinatesArray[j+1].toString()));
+                            if (Integer.parseInt(coordinatesArray[j].toString()) > maxX) {
+                                maxX = Integer.parseInt(coordinatesArray[j].toString());
+                            }
+                            if (Integer.parseInt(coordinatesArray[j].toString()) < minX) {
+                                minX = Integer.parseInt(coordinatesArray[j].toString());
+                            }
+                            if (Integer.parseInt(coordinatesArray[j + 1].toString()) > maxY) {
+                                maxY = Integer.parseInt(coordinatesArray[j + 1].toString());
+                            }
+                            if (Integer.parseInt(coordinatesArray[j + 1].toString()) < minY) {
+                                minY = Integer.parseInt(coordinatesArray[j + 1].toString());
+                            }
                         }
                         j++;
                     }
-                    p2.setColor(0x55661100);
-                    polyPath2.lineTo(Integer.parseInt(coordinatesArray[0].toString()),Integer.parseInt(coordinatesArray[1].toString()));
-                    canvas.drawPath(polyPath2, p2);
+                    p.setColor(0xdd222222);
+                    p.setStyle(Paint.Style.FILL);
+                    p.setStrokeWidth(1);
+                    p.setTextSize(18);
+                    p.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+                    canvas.drawText(rooms[i].getString("name"), (minX + maxX) / 2 - ((maxX - minX) / 4), (minY + maxY) / 2, p);
+
+                    p.setStyle(Paint.Style.STROKE);
+                    p.setStrokeWidth(3);
+                    polyPath.lineTo(Integer.parseInt(coordinatesArray[0].toString()), Integer.parseInt(coordinatesArray[1].toString()));
+                    canvas.drawPath(polyPath, p);
                     polyPath.close();
                 }
+            } catch (Throwable t) {
+            }
+
+
+
+            //zakreslanie wyszczegolnionego pomieszczenia w parametrze funkcji
+            Paint p2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+            Path polyPath2 = new Path();
+            try {
+                dataJSON = new JSONObject(data);
+                JSONArray array = dataJSON.getJSONArray("rooms");
+                JSONObject[] rooms = new JSONObject[array.length()];
+
+                for (int i = 0; i < array.length(); i++) {
+                    p2.setStrokeWidth(4);
+                    p2.setStyle(Paint.Style.FILL);
+
+                    rooms[i] = array.getJSONObject(i);
+                    coordinatesString = rooms[i].getString("coordinates");
+                    coordinatesString = rooms[i].getString("coordinates");
+                    coordinatesString = coordinatesString.substring(2, coordinatesString.length() - 2);
+                    String[] coordinatesArray = coordinatesString.split(",");
+
+                    for (int j = 0; j < coordinatesArray.length; j++) {
+
+                        if (coordinatesArray[j].startsWith("[")) {
+                            coordinatesArray[j] = coordinatesArray[j].substring(1);
+                        } else if (coordinatesArray[j].endsWith("]")) {
+                            coordinatesArray[j] = coordinatesArray[j].substring(0, coordinatesArray[j].length() - 1);
+                        }
+                    }
+                    if (rooms[i].getString("name").equals(room)) {
+                        for (int j = 0; j < coordinatesArray.length; j++) {
+                            if (j == 0) {
+                                polyPath2.moveTo(Integer.parseInt(coordinatesArray[j].toString()), Integer.parseInt(coordinatesArray[j + 1].toString()));
+                            } else {
+                                polyPath2.lineTo(Integer.parseInt(coordinatesArray[j].toString()), Integer.parseInt(coordinatesArray[j + 1].toString()));
+                            }
+                            j++;
+                        }
+                        p2.setColor(0x55661100);
+                        polyPath2.lineTo(Integer.parseInt(coordinatesArray[0].toString()), Integer.parseInt(coordinatesArray[1].toString()));
+                        canvas.drawPath(polyPath2, p2);
+                        polyPath.close();
+                    }
+
+                }
+            } catch (Throwable t) {
 
             }
-        }
-        catch(Throwable t) {
+
 
         }
-    }
+
+
+
 }
